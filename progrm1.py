51c4cbe801102f4c5dca5e5afb3dbf1911e3e0d0
Pritam Kabiraj <72853545+pritam587@users.noreply.github.com>
2021-11-11 07:05:35 -0800

    This is a motivational dictonary that genarate randome motivation code
    This is a motivational dictonary that genarate randome motivation code

---
+++ progrm1.py
@@ -0,0 +1,31 @@
+import random
+d={1: "If you want to achieve greatness stop asking for permission." "--Anonymous",
+2: "Things work out best for those who make the best of how things work out." ,
+3: "To live a creative life, we must lose our fear of being wrong." ,
+4: "If you are not willing to risk the usual you will have to settle for the ordinary." ,
+5: "Trust because you are willing to accept the risk, not because it's safe or certain." ,
+6: "Take up one idea. Make that one idea your life--think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body, be full of that idea, and just leave every other idea alone. This is the way to success." ,
+7: "All our dreams can come true if we have the courage to pursue them." ,
+8: "Good things come to people who wait, but better things come to those who go out and get them." ,
+9: "If you do what you always did, you will get what you always got." ,
+10: "Success is walking from failure to failure with no loss of enthusiasm." ,
+11: "Just when the caterpillar thought the world was ending, he turned into a butterfly." ,
+12: "Successful entrepreneurs are givers and not takers of positive energy." ,
+13: "Whenever you see a successful person you only see the public glories, never the private sacrifices to reach them." ,
+14: "Opportunities don't happen, you create them." ,
+15: "Try not to become a person of success, but rather try to become a person of value.",
+16: "Great minds discuss ideas; average minds discuss events; small minds discuss people." ,
+17: "I have not failed. I've just found 10,000 ways that won't work." ,
+18: "If you don't value your time, neither will others. Stop giving away your time and talents--start charging for it." ,
+19: "A successful man is one who can lay a firm foundation with the bricks others have thrown at him." ,
+20: "No one can make you feel inferior without your consent." ,
+21: "The whole secret of a successful life is to find out what is one's destiny to do, and then do it." ,
+22: "If you're going through hell keep going." ,
+23: "The ones who are crazy enough to think they can change the world, are the ones who do." ,
+24: "Don't raise your voice, improve your argument." ,
+25: "What seems to us as bitter trials are often blessings in disguise." ,
+26: "The meaning of life is to find your gift. The purpose of life is to give it away."
+}
+
+for i in range(1,26):
+    print(d[i])
